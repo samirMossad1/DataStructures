48abaefb4bb1cdd23a7d831471134e2f25f39bca
samirMossad1 <you@example.com>
2019-03-02 10:20:12 +0200

    Added the queue array based interface

---
+++ queue.h
@@ -0,0 +1,68 @@
+/*
+ * queue.h
+ *
+ *  Created on: Mar 2, 2019
+ *      Author: Samir Mossad
+ */
+
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+
+
+
+#define MAX_QUEUE 	 3
+#define QUEUE_START	 0
+#define FALSE		 0u
+#define TRUE 		 1U
+#define NULL 		((void *) 0)
+
+typedef char bool;
+
+
+typedef struct queueitem
+{
+	char Name;
+	int Age;
+	char X;
+
+
+}QueueItem;
+
+
+
+typedef struct queue
+{
+  int first;
+  QueueItem item[MAX_QUEUE];
+
+}Queue;
+
+
+
+
+
+void Queue_init(Queue*);
+
+void Queue_push(const QueueItem*,Queue*);
+
+void Queue_pop(QueueItem*,Queue*);
+
+void Queue_retrieveFirst(QueueItem*,const Queue*);
+
+void Queue_display(const Queue* , void (*ptr)(QueueItem*));
+
+void Queue_Clear(Queue*);
+
+int Queue_size(const Queue*);
+
+bool Queue_emtpy(const Queue*);
+
+bool Queue_full(const Queue*);
+
+
+
+
+
+
+#endif /* QUEUE_H_ */
